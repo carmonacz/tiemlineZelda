abfd8f80c8228b690a0e6a16d0e42bf5df161e50
Jose <carmonajcz@gmail.com>
2023-03-24 22:36:50 +0100

    Add Archivo JS y modificaciones del HTML y CSS

---
+++ script.js
@@ -0,0 +1,7 @@
+function mostrarPopup() {
+  document.querySelector("#popup").classList.remove("oculto");
+}
+
+function ocultarPopup() {
+  document.querySelector("#popup").classList.add("oculto");
+}
