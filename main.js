983136c3842028fcca23af0462b9cb40dbce904d
AyozeJesus <ayozejesusleonsantos@gmail.com>
2023-03-26 02:22:42 +0100

    Esto seria lo que es la modularizacíon con el formulario y los li metidos por dom

---
+++ main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+import {
+  crearFormulario,
+  mostrarPopup,
+  ocultarPopup,
+  getData,
+  mostrarJuegos,
+} from "./helpers.js";
+
+crearFormulario();
+mostrarPopup();
+ocultarPopup();
+mostrarJuegos();
