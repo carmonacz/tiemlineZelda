3735de5b80ea35730fd5e6bb91be166384ec3311
AyozeJesus <ayozejesusleonsantos@gmail.com>
2023-03-27 19:24:05 +0100

    Todos los archivos con el HTML final

---
+++ main.js
@@ -0,0 +1,25 @@
+"use strict";
+
+import {
+  juegos,
+  titulos,
+  fechas,
+  textos,
+  imagenes,
+  lista,
+  crearFormulario,
+  crearLista,
+  eventosParaFechas,
+  crearEventosenlista,
+  mostrarPopup,
+  ocultarPopup,
+  getData,
+  mostrarJuegos,
+} from "./helpers.js";
+
+crearFormulario();
+mostrarPopup();
+ocultarPopup();
+mostrarJuegos();
+crearLista();
+crearEventosenlista();
